48dfbe569e2167ec1f2a42dfca695a52373c9c31
Zohaib Khan <dev.zohaib.work@gmail.com>
2025-03-11 08:02:12 +0500

    chore(web): setup tailwindcss with shadcn

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import config from "@quick-bite/app-config";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "../api/public",
-    emptyOutDir: true,
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  plugins: [react()],
   server: {
     proxy: {
       "/api": `http://localhost:${config.API_PORT}`,
